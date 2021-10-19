ac5f3eda8a9f1e713f49a53d4ca2b2cdb4ada7b5
Marcelo Vasconcellos <marcelomdevasconcellos@gmail.com>
2021-10-19 10:40:15 -0400

    update read_images

--- read_images.py
+++ read_images.py
@@ -37,6 +37,13 @@ def get_image_from_url(url):
 def get_image_from_file(filename):
     return Image.open(filename)
 
+def treat_text(text):
+    text = text.replace('\n\n', '<p>')
+    text = text.replace('\n', ' ')
+    text = text.replace('<p>', '\n\n')
+    while '  ' in text: text = text.replace('  ', ' ')
+    return text.strip()
+
 
 def ocr_image(image):
     return pytesseract.image_to_string(image)
@@ -51,12 +58,25 @@ if __name__ == '__main__':
     print(os.getcwd())
     for file in files:
         input_file = 'input/%s' % file
-        print(input_file)
         if is_image(input_file):
             for a in IMAGE_EXTENSIONS:
                 if a in file.lower():
                     output_file = 'output/%s' % file.replace(a, '.txt')
             image = get_image_from_file(input_file)
             content = ocr_image(image)
-            save_file(output_file, content)
+            save_file(output_file, treat_text(content))
             sys.stdout.write("%s OK!\n" % file)
+        elif os.path.isdir(input_file):
+            print(f'{input_file} is dir')
+            output_file = 'output/%s.txt' % file
+            content = ''
+            subfiles = os.listdir(input_file)
+            subfiles.sort()
+            for subfile in subfiles:
+                input_file = 'input/%s/%s' % (file, subfile)
+                if is_image(input_file):
+                    print(input_file)
+                    image = get_image_from_file(input_file)
+                    content += ocr_image(image).strip()
+            save_file(output_file, treat_text(content))
+            sys.stdout.write("%s OK!\n" % output_file)
